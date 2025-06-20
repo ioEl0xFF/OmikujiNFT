06b864f2e9aa22a700ac3b2498608fa336c95229
Takeru Yamakawa <21017@daitron.co.jp>
2025-06-20 11:17:53 +0900

    コントラクトアドレスを新しいアドレスに更新

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const CONTRACT_ADDRESS = '0xb321508426133033848536E1B3233cC12295A152';
+export const CONTRACT_ADDRESS = '0xf473d52ac2998a8905ee37734037c300838133a0';
 export const IPFS_GATEWAY = 'https://gateway.lighthouse.storage/ipfs/';
